2b37c57b36c38c03db548306368534c52de48ea0
stuart_robert27 <stuart_robert27@hotmail.com>
2023-02-18 18:42:17 +0000

    html tree rendered using data from rust side

--- src/tree.rs
+++ src/tree.rs
@@ -4,24 +4,25 @@ use serde::Serialize;
 
 /// Simple Tree struct - we borrow the values just to put a structure on them
 #[derive(Serialize)]
-pub struct TreeNode<'a, T: Serialize> {
-    pub value: &'a T,
-    pub children: Vec<TreeNode<'a, T>>,
+pub struct PrimeTreeNode {
+    pub key: i32,
+    pub label: String,
+    pub children: Vec<PrimeTreeNode>,
 }
 
 
-impl <'a, T: Serialize> TreeNode<'a, T> {
+impl PrimeTreeNode {
 
     // Creates a new tree node 
-    pub fn new (value: &'a T) -> TreeNode<'a, T> {
-        Self {value: value, children: Vec::new()}
+    pub fn new (key: i32, label: String) -> Self {
+        Self {key: key, label: label, children: Vec::new()}
     }
 
 
     // Adds a child to the tree
-    pub fn add_child (&mut self, value: &'a T) -> &mut TreeNode<'a, T> {
+    pub fn add_child (&mut self, key: i32, label: String) -> &mut Self {
 
-        let new_node = Self {value: value, children: Vec::new()};
+        let new_node = Self {key: key, label: label, children: Vec::new()};
 
         self.children.push(new_node);
         self.children.last_mut().unwrap()
